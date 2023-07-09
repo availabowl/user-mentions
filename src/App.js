b7f8f2f76a518d9f1e2d2434cad5a8b245cca071
avocados23 <namht@vt.edu>
2023-07-09 02:47:26 -0400

    added production variable

--- src/App.js
+++ src/App.js
@@ -1,55 +1,18 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Navbar, Nav, Alert, Form } from 'react-bootstrap';
-import Fuse from 'fuse.js'
+import { Container, Row, Col, Navbar, Nav, Alert } from 'react-bootstrap';
 
 import UserMentionsInput from './UserMentionsInput';
 import './App.css';
 
-let userBank = [
-    {
-        name: "bob"
-    },
-    {
-        name: "apple"
-    },
-    {
-        name: "trevor"
-    },
-    {
-        name: "thomas"
-    },
-	{
-		name: "trevor2"
-	},
-	{ 
-		name: "travis"
-	},
-    {
-        name: "billybob"
-    },
-    {
-        name: "evan"
-    }
-];
-
-function searchForUser(value) {
-    const options = {
-        includeScore: true,
-        shouldSort: true,
-        threshold:.35,
-        isCaseSensitive:false,
-        keys: ['name']
-    }
-
-    let fuse = new Fuse(userBank, options);
-    let result = fuse.search(value);
-    return result;
-}
+async function fetchUsers(query) {
+    const data = await fetch(`${process.env.REACT_APP_PRODUCTION ? "https://rmdh-app.herokuapp.com" : "http://localhost:3002"}/api/user/search?text=${query}&onlyUsers=true`, { json: true }).then(res => { return res.json() })
+    .catch(err => console.error(err));
+    return data;
+};
 
 const App = () => {
 
 	const [usernameMentions, setUsernameMentions] = useState([]);
-    const [preventSelfTagging, setPreventSelfTagging] = useState(false);
 
 	return (
 		<>
@@ -69,36 +32,18 @@ const App = () => {
             <Container style={{marginTop:'48px'}}>
                 <Row>
                     <Col>
-                        <h4 style={{marginBottom:'24px'}}>Availabowl Mentioning Component, v1.7 -- sandbox</h4>
+                        <h4 style={{marginBottom:'24px'}}>Mentioning Component, v1.8.0 Sandbox</h4>
                         <UserMentionsInput 
-                        searchForUser={searchForUser}
+                        searchForUser={fetchUsers}
                         usernameMentions={usernameMentions}
                         setUsernameMentions={setUsernameMentions}
-                        preventSelfTagging={preventSelfTagging}
-                        currentUser={"evan"}
                         />
-                        <Form style={{marginTop:'24px'}}>
-                            <Form.Check type="checkbox" label="Prevent self-tagging (current user is evan)" onClick={() => setPreventSelfTagging(!preventSelfTagging)} />
-                        </Form>
                     </Col>
                 </Row>
                 <Row>
                     <Col style={{marginBottom:'24px'}}>
                         <Alert variant="secondary" style={{marginTop:'24px'}}>
                             <b>Saved username mentions</b>: {JSON.stringify(usernameMentions)}
-                        </Alert>
-                        <p style={{marginTop:'24px'}}><b>Users in dummy database</b></p>
-                        {
-                            userBank.map((user, i) => <p style={{fontFamily:"monospace"}} key={i}>{JSON.stringify(user)}</p>)
-                        }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <p><b>Disclaimer</b></p>
-                        <Alert variant="warning">
-                            Self-tagging can still occur if the username where typing @evan was a match while preventing it was changed during such. Since the case of making self-tagging
-                            being toggleable on the client side is a rare situation, this is not seen as an issue.
                         </Alert>
                     </Col>
                 </Row>
--- src/UserMentionsInput.js
+++ src/UserMentionsInput.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef, useCallback } from 'react';
 import { Form, ListGroup, Image } from 'react-bootstrap';
 
+const defaultImgUrl = "https://bucketeer-ba012f24-3291-4642-8fd0-677ac237a075.s3.amazonaws.com/public/u1z79m6E4o6f-9m66f2UaUloy-ecarmda1lZIx_pfp.jpg";
+
 const UserPfpComponent = ({user}) => {
-    return <Image src="https://bucketeer-ba012f24-3291-4642-8fd0-677ac237a075.s3.amazonaws.com/public/u1z79m6E4o6f-9m66f2UaUloy-ecarmda1lZIx_pfp.jpg" style={{width:"36px",height:"36px",position:"relative",top:"3px",zIndex:30,backgroundColor:"#222"}} 
+    return <Image src={user.imageUrl ? user.imageUrl : defaultImgUrl} style={{width:"36px",height:"36px",position:"relative",top:"3px",zIndex:30,backgroundColor:"#222"}} 
     roundedCircle alt={user.name} />
 };
 
@@ -11,69 +13,73 @@ const UserListGroup = ({user}) => {
         <div className="d-flex">
             <UserPfpComponent user={user} />
             <div style={{marginLeft:'16px'}}>
-                <p style={{lineHeight:'36px',marginBottom:0}}>{user.name}</p>
+                <p style={{lineHeight:'36px',marginBottom:0}}>{user.username}</p>
             </div>
         </div>
     )
 }
 
-const UserMentionsInput = ({searchForUser, usernameMentions, setUsernameMentions, preventSelfTagging, currentUser}) => {
-
-    const pattern = /@(?=.{2,20}(?:\s|$))[a-z][a-z0-9]+(?:[_][a-z0-9]+)?/ig;
+const UserMentionsInput = ({searchForUser, usernameMentions, setUsernameMentions}) => {
     const textareaRef = useRef(null);
+    const pattern = /@(?=.{2,20}(?:\s|$))[a-z][a-z0-9]+(?:[_][a-z0-9]+)?/ig;
 
     const [focusedValue, setFocusedValue] = useState(null);
     const [showResults, setShowResults] = useState(false);
     const [results, setResults] = useState([]);
 
     const [textareaContent, setTextareaContent] = useState('');
-    const [cursorPosition, setCursorPosition] = useState(0);
+    const [whiteSpaceIndexes, setWhiteSpaceIndexes] = useState([]);
+
+    const [positions, setPositions] = useState(new Map());
 
     const handleDeleteUsernameMentions = useCallback((textContent) => {
-        usernameMentions.forEach((username, i) => { // Go through each saved username in the state array
-            if (textContent.indexOf(`@${username}`) === -1) { // If @username is no longer in the textarea's content, remove it and update state array
+        usernameMentions.forEach((username, i) => {
+            if (textContent.indexOf(`@${username}`) === -1) {
                 let temp = [...usernameMentions.slice(0,i), ...usernameMentions.slice(i+1)];
                 setUsernameMentions(temp);
+                let tempMap = new Map(positions);
+                tempMap.delete(`@${username}`);
+                setPositions(tempMap);
             }
-        })
-    }, [usernameMentions, setUsernameMentions]);
+        });
+    }, [usernameMentions, setUsernameMentions, positions]);
 
-    const handleUsernameMentions = useCallback((textContent) => {
-        let usernameDetections = textContent.substring(0, cursorPosition).match(/@(?=.{2,20}(?:\s|$))[a-z][a-z0-9]+(?:[_][a-z0-9]+)?/ig) || [];
+    const handleUsernameMentions = useCallback(async (textContent) => {
+        let usernameDetections = textContent.match(/@(?=.{1,20}(?:\s|$))[a-z][a-z0-9]+(?:[_][a-z0-9]+)?/ig) || [];
+
+        if (showResults && usernameDetections.length === 0) {
+            setShowResults(false);
+        }
 
         if (usernameDetections.length > 0) {
-            let focusedUserValue = usernameDetections.slice(-1)[0].substring(1);
-            setFocusedValue(focusedUserValue);
-
-            let endIndexOfFocusedUserValue = textContent.indexOf(focusedUserValue) + focusedUserValue.length;
-
-            if (!usernameMentions.includes(focusedUserValue) && textContent.substring(endIndexOfFocusedUserValue, endIndexOfFocusedUserValue+1) !== ' ') {                
-                let prelimSearchResults = searchForUser(focusedUserValue);
-
-                if (prelimSearchResults.length > 0 && !usernameMentions.includes(prelimSearchResults[0].item.name)) {
-                    if (preventSelfTagging && currentUser !== undefined) {
-                        prelimSearchResults = prelimSearchResults.filter(result => result.item.name !== currentUser);
-                    }
-
-                    // Prevent any duplicates from being inputted.
-                    prelimSearchResults = Array.from(new Set(prelimSearchResults))
-
-                    setResults(prelimSearchResults);
-                    setShowResults(true);    
-                }
-            }        
+            setFocusedValue(usernameDetections[0]);
+            let prelimSearchResults = await searchForUser(usernameDetections[0]);
+            if (!positions.has(usernameDetections[0])) {
+                setResults(prelimSearchResults);
+                setShowResults(true);
+            }
         }
-    }, [currentUser, preventSelfTagging, searchForUser, usernameMentions, cursorPosition]);
+
+    }, [searchForUser, setResults, positions, showResults]);
 
     const handleOnChange = async (e) => {
-        let textareaContent = e.target.value.substring(0, textareaRef.current.selectionStart);
         setTextareaContent(e.target.value);
-        setCursorPosition(textareaRef.current.selectionStart);
 
-        handleUsernameMentions(textareaContent);
+        let temp = [];
+        e.target.value.replace(/\s/g, function(_, index) {
+            temp.push(index);
+        });
+        setWhiteSpaceIndexes(temp);
+
+        let textareaContentTemp = e.target.value.substring(0, textareaRef.current.selectionStart);
         
-        if (textareaContent.substring(textareaContent.length-1) === ' ' || textareaContent.length === 0) {
+        if (textareaContentTemp.substring(textareaContentTemp.length-1) === ' ' || textareaContentTemp.length === 0) {
             setShowResults(false);
+            setFocusedValue(null);
+            setResults([]);
+        } else {
+            let bounds = await determineSubstringBounds(textareaRef.current.selectionStart);
+            handleUsernameMentions(e.target.value.substring(bounds.start, bounds.end).trim());
         }
 
         return new Promise((resolve, _) => {
@@ -81,7 +87,128 @@ const UserMentionsInput = ({searchForUser, usernameMentions, setUsernameMentions
         });
     };
 
-    const handleChainedOnchange = (e) => {
+    const handleCompleteMention = async (username) => {
+        let bounds = await determineSubstringBounds(textareaRef.current.selectionStart);
+        
+        let newlyAppendedUsername = textareaContent.substring(0, bounds.start) + '@' + username;
+        let newlyEndedTextareaPosition = bounds.start+username.length+1;
+
+        let appendedUserTotextareaContent = newlyAppendedUsername;
+        
+        if (textareaContent.substring(newlyEndedTextareaPosition, newlyEndedTextareaPosition+1) !== ' ') {
+            appendedUserTotextareaContent += ' ';
+        }
+
+        if (newlyEndedTextareaPosition !== textareaContent.length) {
+            appendedUserTotextareaContent += textareaContent.substring(newlyEndedTextareaPosition);
+        }
+
+        textareaRef.current.value = appendedUserTotextareaContent;
+        setTextareaContent(appendedUserTotextareaContent);
+
+        setUsernameMentions(Array.from(new Set([...usernameMentions, username])));
+
+        if (positions.has(`@${username}`)) {
+            setPositions(new Map(positions).set(`@${username}`, [...positions.get(`@${username}`), bounds.start]));
+        } else {
+            setPositions(new Map(positions).set(`@${username}`, [bounds.start]));
+        }
+
+        textareaRef.current.focus();
+        setShowResults(false);
+        setFocusedValue(null);        
+
+        return new Promise((resolve, _) => {
+            resolve(textareaContent);
+        });
+
+    };
+
+    const determineSubstringBounds = async (currentCursorPosition) => {
+        // Case 1: no whitespaces
+        if (whiteSpaceIndexes.length === 0) {
+            return { start: 0 }
+        }
+        // Case 2a: one whitespace
+        if (whiteSpaceIndexes.length === 1) {
+            if (textareaContent.substring(textareaContent.length-1) === ' ') { // Case 2a: located at end
+                return { start: 0 };
+            } else if (textareaContent.indexOf(' ') !== textareaContent.length-1) { // Case 2b: in between
+                if (currentCursorPosition <= whiteSpaceIndexes[0]) { // Case 2b.1: cursor is prior to the whitespace in between
+                    return { start: 0, end: whiteSpaceIndexes[0] };
+                } else { // Case 2b.2: cursor is after the whitespace in between
+                    return { start: whiteSpaceIndexes[0]+1 }
+                }
+            }
+        }
+
+        // Case 3: multiple whitespaces (greater than 1)
+        for (let i = 0; i < whiteSpaceIndexes.length; i++) {
+            if (i === 0 && currentCursorPosition < whiteSpaceIndexes[i]) {
+                return { start: 0, end: whiteSpaceIndexes[i]}
+            }
+            if (i === 0 && currentCursorPosition > whiteSpaceIndexes[i] && currentCursorPosition < whiteSpaceIndexes[i+1]) {
+                console.log('3a.0')
+                return { start: whiteSpaceIndexes[i]+1, end: whiteSpaceIndexes[i+1] }
+            } else if (i === whiteSpaceIndexes.length-1) {
+                if (textareaContent.substring(textareaContent.length-1) === ' ') { // Case 3a.1: the whitespace IS the last character
+                    console.log('3a.1')
+                    // Don't want to evaluate anything, honestly.
+                } else { // Case 3a.2: the whitespace IS NOT the last character
+                    // Evaluate what's after.
+                    console.log('3a.2')
+                    return { start: whiteSpaceIndexes[i]+1 }
+                }
+            } else if (whiteSpaceIndexes[i] <= currentCursorPosition && whiteSpaceIndexes[i+1] > currentCursorPosition) {
+                // Need to differentiate when to look after, or look before.
+                if (i < whiteSpaceIndexes.length-1 && i !== 0) {
+                    console.log('4a')
+                    return { start: whiteSpaceIndexes[i-1]+1, end: whiteSpaceIndexes[i] }
+                }
+                console.log('4b')
+                return { start: whiteSpaceIndexes[i]+1, end: whiteSpaceIndexes[i+1] }
+            }
+        }
+        return { start: 0, end: textareaContent.indexOf(' ') !== -1 ?  textareaContent.indexOf(' ') : undefined };
+    }
+
+    const handleOnClick = async (e) => {
+        let currentCursorPosition = textareaRef.current.selectionStart; 
+        let bounds = await determineSubstringBounds(currentCursorPosition);
+        handleUsernameMentions(e.target.value.substring(bounds.start, bounds.end));
+    };
+
+    const handlePriorDeleteUsernameMention = async (e) => {
+        let newString = e.target.value;
+
+        if (e.keyCode === 8 || e.keyCode === 46) {
+            let bounds = await determineSubstringBounds(textareaRef.current.selectionStart);
+            let username = e.target.value.substring(bounds.start, bounds.end).trim().match(pattern) || [];
+            if (username[0]) {
+                let savedUserMentionPositions = positions.get(username[0]);
+
+                if (savedUserMentionPositions !== undefined) {
+                    for (let i = 0; i < savedUserMentionPositions.length; i++) {
+                        let startIndex = savedUserMentionPositions[i];
+                        let endIndex = startIndex+username[0].length;
+                        if (textareaRef.current.selectionStart > startIndex && textareaRef.current.selectionStart <= endIndex) {
+                            newString = e.target.value.substring(0, startIndex) + e.target.value.substring(endIndex+1);
+                            textareaRef.current.value = newString;
+                            handleDeleteUsernameMentions(newString);
+                            return new Promise((resolve, _) => {
+                                resolve(startIndex);
+                            });
+                        }
+                    }
+                }  
+            }
+        }
+        return new Promise((resolve, _) => {
+            resolve(-1);
+        });
+    };
+
+    const handleChainedOnchange = async (e) => {
         handleOnChange(e).then(textContent => {
             handleDeleteUsernameMentions(textContent);
         })
@@ -93,63 +220,41 @@ const UserMentionsInput = ({searchForUser, usernameMentions, setUsernameMentions
         })
     };
 
-    const handleCompleteMention = (username) => {
-        let usernameDetections = textareaContent.substring(0, cursorPosition).match(pattern) || [];
-        let mostRecentMention = usernameDetections[usernameDetections.length-1];
-        let index = textareaContent.indexOf(mostRecentMention);
-
-        let newlyAppendedUsername = textareaContent.substring(0, index) + '@' + username;
-        let newlyEndedTextareaPosition = index+mostRecentMention.length;
-
-        let appendedUserTotextareaContent = newlyAppendedUsername;
-        if (textareaContent.substring(newlyEndedTextareaPosition, newlyEndedTextareaPosition+1) !== ' ') {
-            appendedUserTotextareaContent += ' ';
-        }
-
-        if (newlyEndedTextareaPosition !== textareaContent.length) {
-            appendedUserTotextareaContent += textareaContent.substring(newlyEndedTextareaPosition);
-        }
-
-        textareaRef.current.value = appendedUserTotextareaContent;
-
-        setUsernameMentions([...usernameMentions, username]);
-        textareaRef.current.focus();
-        setShowResults(false);
-        setFocusedValue(null);
-
-        return new Promise((resolve, _) => {
-            resolve(textareaContent);
-        });
-    };
-
-    const handleOnClick = (e) => {
-        setCursorPosition(textareaRef.current.selectionStart);
-        handleUsernameMentions(e.target.value.substring(0, textareaRef.current.selectionStart))
+    const handleChainedPriorDeleteUsernameMention = (e) => {
+        handlePriorDeleteUsernameMention(e).then(value => {
+            if (value !== -1) {
+                textareaRef.current.selectionStart = value;
+                textareaRef.current.selectionEnd = value;
+            }
+        })
     };
 
     return (
-        <div style={{position:"relative"}}>
-            <Form>
-                <Form.Control as="textarea" rows={5} style={{resize:'none'}} ref={textareaRef}
-                onChange={handleChainedOnchange}
-                onBlur={e => setTextareaContent(e.target.value)}
-                onClick={handleOnClick}
-                placeholder="Enter some text here."
-                />
-                {
-                    showResults && focusedValue !== null && results.length > 0 &&
-                    <ListGroup style={{position:"absolute",zIndex:999,width:"300px",height:"200px",overflowY:"auto",bottom:-225,right:0}}>
-                        {
-                            results.map((result) =>
-                                <ListGroup.Item key={result.item.name} action onClick={() => handleChainedCompleteMention(result.item.name)}>
-                                    <UserListGroup user={result.item}/>
-                                </ListGroup.Item>
-                            )
-                        }
-                    </ListGroup>
-                }
-            </Form>
-        </div>
+        <>
+            <div style={{position:"relative"}}>
+                <Form>
+                    <Form.Control as="textarea"
+                    onClick={handleOnClick}
+                    className="user-input form-control" ref={textareaRef}
+                    onInput={handleChainedOnchange}
+                    onBlur={e => setTextareaContent(e.target.value)}
+                    onKeyDown={handleChainedPriorDeleteUsernameMention}
+                    />
+                    {
+                        showResults && focusedValue !== null && results.length > 0 &&
+                        <ListGroup style={{position:"absolute",zIndex:999,width:"300px",height:"200px",overflowY:"auto",bottom:-225,right:0}}>
+                            {
+                                results.map((result) =>
+                                    <ListGroup.Item key={result.username} action onClick={() => handleChainedCompleteMention(result.username)}>
+                                        <UserListGroup user={result}/>
+                                    </ListGroup.Item>
+                                )
+                            }
+                        </ListGroup>
+                    }
+                </Form>
+            </div>
+        </>
     )
 };
 
